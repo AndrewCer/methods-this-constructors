408df1679864f1918a9ad0c59e776922480a8b62
Andrew Cer <cer.andrew@gmail.com>
2015-07-13 16:47:06 -0600

    all tests passing

--- book_reader.js
+++ book_reader.js
@@ -1,21 +1,50 @@
-function BookReader(book) {
+function BookReader(book, currentPage) {
   // your code here
+  this.book = book,
+  this.currentPage = 0;
 }
 
 BookReader.prototype.nextPage = function() {
   // your code here
+  if (this.book.length - 1 > this.currentPage) {
+    this.currentPage += 1;
+    var indexOfBook = this.currentPage;
+    return this.book[indexOfBook];
+  }
+  if (this.book.length - 1 === this.currentPage) {
+    var indexOfBook = this.currentPage;
+    return this.book[indexOfBook];
+  }
 };
 
 BookReader.prototype.previousPage = function() {
   // your code here
+  if (this.currentPage === 0) {
+    var indexOfBook = this.currentPage;
+    return this.book[indexOfBook];
+  }
+  else {
+    this.currentPage = this.currentPage - 1;
+    var indexOfBook = this.currentPage;
+    return this.book[indexOfBook];
+  }
 };
 
 BookReader.prototype.pagesLeft = function() {
   // your code here
+  return (this.book.length - 1) - this.currentPage
 };
 
 BookReader.prototype.encouragement = function() {
   // your code here
+  var pageRemainder = (this.book.length - 1) - this.currentPage;
+  if (pageRemainder === 1) {
+    return "Keep going, this book is good 'til the last drop!";
+  }
+  else {
+    var string = "Keep going, only " + pageRemainder + " pages left after this one!";
+    return string;
+  }
 };
 
 // DO NOT MODIFY BELOW THIS COMMENT:
--- taxi.js
+++ taxi.js
@@ -1,13 +1,36 @@
 function Taxi(driverName, maxNumberOfPassengers) {
   // your code here
+  this.driverName  = driverName;
+  this.maxNumberOfPassengers = maxNumberOfPassengers;
+  this.passengers = [];
 }
 
 Taxi.prototype.addPassenger = function(passengerName) {
   // your code here
-  
+  this.passengers.push(passengerName);
 };
 
-// your code here
+Taxi.prototype.passengerCount = function () {
+  if (this.passengers.length > 0) {
+    return this.passengers.length;
+  }
+  else {
+    return 0;
+  }
+}
+
+Taxi.prototype.full = function () {
+  if (this.passengers.length >= 4) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+Taxi.prototype.dropOffPassengers = function () {
+  this.passengers.splice(0, this.passengers.length);
+}
 
 // DO NOT MODIFY BELOW THIS COMMENT:
 module.exports = Taxi;
